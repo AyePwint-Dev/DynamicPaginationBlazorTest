c24da81edc4ce268b932e1d13b74fd57aaea03b2
@AyePwint-Dev <ayepwintsa49@gmail.com>
2025-01-30 13:43:20 +0800

    Add project files.

---
+++ BlazorApp1/BlazorApp1/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorApp1
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    //sample dummy data
+    public class ProductService
+    {
+        public List<Product> GetProducts()
+        {
+            // Sample data
+            return Enumerable.Range(1, 100).Select(i => new Product
+            {
+                Id = i,
+                Name = $"Product {i}",
+                Price = i * 10
+            }).ToList();
+        }
+    }
+}
---
+++ DynamicPagination/Model/ProductService.cs
@@ -0,0 +1,17 @@
+﻿namespace DynamicPagination.Model
+{
+    //sample dummy data
+    public class ProductService
+    {
+        public List<Product> GetProducts()
+        {
+            // Sample data
+            return Enumerable.Range(1, 100).Select(i => new Product
+            {
+                Id = i,
+                Name = $"Product {i}",
+                Price = i * 10
+            }).ToList();
+        }
+    }
+}
